a0cd98a55e5d3a0ba497dc67779ef3087adbdbfe
jzawila <jaroslaw.zawila@gmail.com>
2018-12-03 06:20:44 +0000

    Add initial state for square

--- src/main/scala/net/zawila/react/Square.scala
+++ src/main/scala/net/zawila/react/Square.scala
@@ -6,12 +6,15 @@ import japgolly.scalajs.react.extra._
 
 object Square {
 
-  final class Backend($: BackendScope[Unit, Unit]) {
-    def render(p: Unit): VdomElement =
-      <.button(^.className := "Square")
+  case class Props(number: Int)
+
+  final class Backend($: BackendScope[Unit, Props]) {
+    def render(p: Props): VdomElement =
+      <.button(^.className := "Square", p.number)
   }
 
   val Component = ScalaComponent.builder[Unit]("Square")
+    .initialState(Props(1))
     .renderBackend[Backend]
     .build
 }
