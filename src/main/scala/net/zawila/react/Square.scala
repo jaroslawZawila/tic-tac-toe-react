c4c5b7983f356b0fbb29334670a82c54736fb950
jzawila <jaroslaw.zawila@gmail.com>
2018-12-04 05:33:06 +0000

    square is interactive

--- src/main/scala/net/zawila/react/Square.scala
+++ src/main/scala/net/zawila/react/Square.scala
@@ -3,14 +3,22 @@ package net.zawila.react
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
 import japgolly.scalajs.react.extra._
+import org.scalajs.dom
 
 object Square {
 
   case class Props(number: Int)
 
+  def click = Callback{
+    dom.window.alert("click")
+  }
+
   final class Backend($: BackendScope[Props, Props]) {
     def render(p: Props): VdomElement =
-      <.button(^.className := "Square", p.number)
+      <.button(
+        ^.className := "Square",
+        ^.onClick --> click,
+        p.number)
   }
 
   val Component = ScalaComponent.builder[Props]("Square")
