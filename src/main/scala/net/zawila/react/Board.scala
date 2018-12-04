91c1c1a1bb4941df03ac6e769a386cf62ad397c2
jzawila <jaroslaw.zawila@gmail.com>
2018-12-04 19:11:31 +0000

    Users are able to win

--- src/main/scala/net/zawila/react/Board.scala
+++ src/main/scala/net/zawila/react/Board.scala
@@ -8,20 +8,43 @@ object Board  {
 
   case class State(squares: List[String], isXNext: Boolean)
 
+  val winningComibinations = Set(
+    Seq( 0, 1, 2),
+    Seq( 3, 4, 5),
+    Seq( 6, 7, 8),
+    Seq( 0, 3, 6),
+    Seq( 1, 4, 7),
+    Seq( 2, 5, 8),
+    Seq( 0, 4, 8),
+    Seq( 2, 4, 6)
+  )
+
+  def calculateWinner(s: List[String]) = {
+    winningComibinations.foldLeft(Option.empty[String]) { (x, y) =>
+      y match {
+        case a :: b :: c :: Nil => if (s(a) == s(b) && s(a) == s(c)) Some(s(a)) else None
+        case _ => None
+      }
+    }
+  }
+
   final class Backend($: BackendScope[Unit, State]) {
-
-
 
     def handleClick(int: Int) = {
       $.modState(s => {
         val xOrO = if(s.isXNext) "X" else "O"
-        State(s.squares.updated(int, xOrO), !s.isXNext)
+        val square = s.squares(int)
+        if(square != "X" && square != "O") {
+          State(s.squares.updated(int, xOrO), !s.isXNext)
+        } else
+        s
       })
     }
 
     def render(p: Unit, state: State): VdomElement = {
       val xOrO = if(state.isXNext) "X" else "O"
-      val status = s"Next player: $xOrO"
+      val winner = calculateWinner(state.squares)
+      val status = winner.map(w => s"Winner: $w").getOrElse(s"Next player: '$xOrO'")
       <.div(
         <.div(^.className := "status", status),
         <.div(^.className := "board-row",
