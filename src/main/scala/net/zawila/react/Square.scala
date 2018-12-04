e5db553d659bbe2ef274da69f705ea0baf901c77
jzawila <jaroslaw.zawila@gmail.com>
2018-12-04 18:04:26 +0000

    Tidy up Square

--- src/main/scala/net/zawila/react/Square.scala
+++ src/main/scala/net/zawila/react/Square.scala
@@ -2,15 +2,10 @@ package net.zawila.react
 
 import japgolly.scalajs.react._
 import japgolly.scalajs.react.vdom.html_<^._
-import japgolly.scalajs.react.extra._
-import org.scalajs.dom
 
 object Square {
 
   case class Props(number: String, onClick: Callback)
-  case class State(number: String)
-
-
 
   final class Backend($: BackendScope[Props, Unit]) {
 
